76495a7ee92c1ec0d74811aa5bd487c7f2691854
Luloo-Reg <Luizaregnier@tecgraf.puc-rio.br>
2024-10-28 14:32:09 -0300

    que odiooo do assync função pra verificar o usuário ta funcionando

---
+++ scripts/login.js
@@ -0,0 +1,31 @@
+
+  async function verificarUsuario() {
+    const login = document.getElementById("login").value;
+    const senha = document.getElementById("senha").value;
+
+    try {
+        const response = await fetch('scripts/loginUsuarios.json');
+        const usuarios = await response.json();
+
+        const usuarioEncontrado = usuarios.find(usuario => 
+            usuario.login === login && usuario.senha === senha
+        );
+
+        if (usuarioEncontrado) {
+            alert("Login bem-sucedido!");
+        } else {
+            alert("Login ou senha incorretos. Tente novamente.");
+        }
+    } catch (error) {
+        console.error("Erro ao carregar o arquivo JSON:", error);
+        alert("Erro ao carregar dados de login.");
+    }
+}
+
+window.addEventListener("load", function() {
+    const submitButton = document.querySelector("#loginForm button[type='submit']");
+    submitButton.addEventListener("click", function(event) {
+        event.preventDefault(); 
+        verificarUsuario();
+    });
+});
