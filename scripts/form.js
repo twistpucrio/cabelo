d60454adb5e2f381bcdbde771e63588008b393ad
FernandaFigueirdo <fernandacalfig@tecgraf.puc-rio.br>
2024-11-12 15:28:08 -0300

    html form

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,24 @@
 function gerarRotina() {
-    const procedimento = document.getElementById('procedimento').value;
     const preocupacao = document.getElementById('preocupacao').value;
-    const ferramentasCalor = document.getElementById('ferramentasCalor').value;
 
     let rotinaRecomendada = '';
 
-    if ((procedimento === "Com Luzes" || procedimento === "Colorido" ||  procedimento === "Alisado" ) &&
-        (preocupacao === "Elástico" || preocupacao === "Queda de cabelo") &&
-        (ferramentasCalor === "Nunca" || ferramentasCalor === "1 a 3 dias")) {
-        rotinaRecomendada = 'Rotina 1: Controle de Frizz';
-    } else if ((procedimento === "Com Luzes" || procedimento === "Colorido") &&
-               (preocupacao === "Opacidade" || preocupacao === "Frizz") &&
-               (ferramentasCalor === "Nunca" || ferramentasCalor === "1 a 3 dias")) {
-        rotinaRecomendada = 'Rotina 2: Queda';
-    } else if ((procedimento === "Colorido"|| procedimento === "Natural" ) &&
-               (preocupacao === "Ressecamento" || preocupacao === "Elasticidade") &&
-               ferramentasCalor === "Todos os dias" ) {
-        rotinaRecomendada = 'Rotina 3: Ressecamento';
-    } else if ((procedimento === "Com Luzes" || procedimento === "Colorido" || procedimento === "Alisado") &&
-               (preocupacao === "Danos e Quebras" || preocupacao === "Opacidade") &&
-               ferramentasCalor === "4 a 6 dias") {
-        rotinaRecomendada = 'Rotina 3: Ressecamento';
-    } else {
+    if ((preocupacao === "Queda de cabelo" || preocupacao === "Elasticidade")) {
+        rotinaRecomendada = 'Rotina 1: Danos e quebras';
+    } 
+   
+    else if ((preocupacao === "Ressecamento")) {
+        rotinaRecomendada = 'Rotina 2: Ressecamento';
+    }
+    
+    else if ((preocupacao === "Frizz")) {
+        rotinaRecomendada = 'Rotina 3: Controle de frizz';
+    } 
+    
+    else if (("Opacidade")) {
+        rotinaRecomendada = 'Rotina 4: Brilho e vitalidade';
+    } 
+    else {
         rotinaRecomendada = 'Nenhuma rotina específica foi encontrada para as suas respostas.';
     }
 
