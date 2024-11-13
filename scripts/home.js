cf0e9da818a57ec9f690fc127b2ada4f1dea932c
Maria Luiza Dutra <132154059+MaluDutra@users.noreply.github.com>
2024-11-13 17:55:58 -0300

    Botão que leva para o formulário e condição se usuário está logado ou não

--- scripts/home.js
+++ scripts/home.js
@@ -1,8 +1,43 @@
+function mostraModal() {
+    let modal = document.getElementById("modalFormulario");
+    let btnFecha = document.getElementById("botaoFecha");
+    let btnLogin = document.getElementById("botaoLogin");
+
+    btnFecha.addEventListener('click', function () {
+        modal.classList.remove("open");
+    });
+
+    btnLogin.addEventListener('click', function () {
+        window.location = "loginCadastro.html";
+    });
+
+    modal.classList.add("open");
+}
+
 window.addEventListener('scroll', function() {
     const navigator = document.getElementById('navigator');
+
     if (window.scrollY > 30) { 
         navigator.classList.add('scrolled');
     } else {
         navigator.classList.remove('scrolled');
     }
+});
+
+window.addEventListener("load", function(){
+    const msgUsuario = document.getElementById('mensagemHome');
+    const formularioBtn = document.getElementById('formularioBtn');
+    let user = JSON.parse(localStorage.getItem("usuario"));
+
+    if (user){
+        msgUsuario.innerHTML = `Olá, ${user.login}!`;
+    } 
+
+    formularioBtn.addEventListener("click", function(){
+        if (user){
+            window.location = "form.html";
+        } else{
+            mostraModal();
+        }
+    });
 });
